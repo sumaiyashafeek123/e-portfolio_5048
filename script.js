289c2968682f6bae045f1118232b3c44168c7d0c
sumaiyashafeek123 <137602367+sumaiyashafeek123@users.noreply.github.com>
2025-04-29 22:03:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+//typing animation*/
+var typed = new Typed(".typing",{
+    strings:["","photographer","photo editor","nature photographer","street photographer","portrait photographer","architectural photographer"],
+    typeSpeed:100,
+    BackSpeed:70,
+    loop:true
+})
